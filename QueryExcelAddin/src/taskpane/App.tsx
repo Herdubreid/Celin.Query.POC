6925ea62dba0018a6e0310a2e29c79ab24a551ba
Finnur Bragason <fbragason@outlook.com>
2021-07-06 12:34:57 +0800

    new functionality

--- QueryExcelAddin/src/taskpane/App.tsx
+++ QueryExcelAddin/src/taskpane/App.tsx
@@ -10,97 +10,90 @@ import {
   MessageBar,
   MessageBarType,
 } from "@fluentui/react";
+import IQueryResponse from "./components/IQueryResponse";
 import CqlEditor from "./components/CqlEditor";
-import IQueryResponse from "./IQueryResponse";
+import { PasteResult } from "./components/pasteResult";
 
 export interface IAppProps {}
 export interface IAppState {
   code: string;
-  warning: string;
   error: string;
   busy: boolean;
+  result: IQueryResponse | null;
 }
 
 export default class App extends React.Component<IAppProps, IAppState> {
-  state = { code: "", warning: "", error: "", busy: false };
+  state = { code: "", warning: "", error: "", busy: false, result: null };
   click = async () => {
-    this.setState({ warning: "", error: "", busy: true });
+    let result: IQueryResponse | null = null;
+    this.setState({ error: "", busy: true, result: result });
     try {
-      await Excel.run(async (context) => {
-        await axios({
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          data: JSON.stringify({
-            code: this.state.code,
-          }),
-        })
-          .then((res) => {
-            const qry: IQueryResponse = res.data;
-            if (qry.Error > "") {
-              this.setState({ error: qry.Error });
-            } else {
-              if (qry.Summary.records > 0) {
-                if (qry.Summary.moreRecords) {
-                  this.setState({ warning: `Results limited to ${qry.Summary.records} rows.` });
-                }
+      await axios({
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        url: "https://celinqueryfunctions.azurewebsites.net/api/Submit?code=ucXWB4Lf4JAYtUSexBwtzKiEZYtumi16SaozU5yxyXP3Easoe6VzqA==",
+        data: JSON.stringify({
+          code: this.state.code,
+        }),
+      })
+        .then(async (res) => {
+          if (res.data.Error > "") {
+            this.setState({ error: res.data.Error });
+          } else {
+            if (res.data.Demo) {
+              // eslint-disable-next-line no-undef
+              await Excel.run(async (context) => {
                 const sheet = context.workbook.worksheets.add();
-                const range = sheet.getRange("A1").getResizedRange(0, qry.Data[0].length - 1);
+                const range = sheet.getRange("A1").getResizedRange(0, res.data.Data[0].length - 1);
                 const table = sheet.tables.add(range, true);
-                const titles = Object.entries(qry.Columns).map((c) => (c[1] === "" ? c[0] : c[1]));
+                const titles = Object.entries(res.data.Columns).map((c) => (c[1] === "" ? c[0] : c[1]));
                 table.getHeaderRowRange().values = [titles];
-                table.rows.add(null, qry.Data);
+                table.rows.add(null, res.data.Data);
                 sheet.getUsedRange().format.autofitColumns();
                 sheet.getUsedRange().format.autofitRows();
                 sheet.activate();
-              } else {
-                this.setState({ warning: "No records returned!" });
-              }
+                await context.sync();
+              });
+            } else {
+              result = res.data;
             }
-          })
-          .catch((err) => {
-            console.error(err);
-          });
-        await context.sync();
-      });
+          }
+        })
+        .catch((error) => {
+          this.setState({ error });
+        });
     } catch (error) {
-      console.error(error);
+      this.setState({ error });
     }
-    this.setState({ busy: false });
+    this.setState({ busy: false, result: result });
   };
   handleCodeChange = (code: string) => this.setState({ code });
   stackStyle: IStackTokens = {
     childrenGap: 10,
+    padding: 5,
   };
   render() {
     let spinner;
     if (this.state.busy) {
       spinner = <Spinner size={SpinnerSize.small} />;
     }
-    let warning;
-    if (this.state.warning > "") {
-      warning = (
-        <StackItem align="center">
-          <MessageBar messageBarType={MessageBarType.warning}>{this.state.warning}</MessageBar>
-        </StackItem>
-      );
-    }
     let error;
     if (this.state.error > "") {
       error = (
-        <StackItem align="center">
+        <StackItem align="start">
           <MessageBar messageBarType={MessageBarType.error}>{this.state.error}</MessageBar>
         </StackItem>
       );
     }
     return (
       <Stack tokens={this.stackStyle}>
+        <PasteResult result={this.state.result} />
         <Stack.Item align="start">
           <PrimaryButton onClick={this.click} disabled={this.state.busy}>
             Run&nbsp;
             {spinner}
           </PrimaryButton>
         </Stack.Item>
-        {warning}
         {error}
         <Stack.Item>
           <CqlEditor code={this.state.code} onValueChange={this.handleCodeChange} />
---
+++ QueryExcelAddin/src/taskpane/components/pasteResult.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { PrimaryButton, Checkbox, Stack, StackItem, IStackTokens, MessageBar, MessageBarType } from "@fluentui/react";
+import IQueryResponse from "./IQueryResponse";
+
+interface IPasteResultProps {
+  result: IQueryResponse | null;
+}
+interface IPasteResultState {
+  includeHeader: boolean;
+}
+
+export class PasteResult extends React.Component<IPasteResultProps, IPasteResultState> {
+  tokens: IStackTokens = {
+    childrenGap: 5,
+  };
+  state = {
+    includeHeader: true,
+  };
+  handleIncludeHeader = () => this.setState({ includeHeader: !this.state.includeHeader });
+  handlePaste = async () => {
+    try {
+      // eslint-disable-next-line no-undef
+      await Excel.run(async (context) => {
+        let startCell = context.workbook.getActiveCell();
+        if (this.state.includeHeader) {
+          const header = startCell.getResizedRange(0, this.props.result.Data[0].length - 1);
+          const titles = Object.entries(this.props.result.Columns).map((c) => (c[1] === "" ? c[0] : c[1]));
+          header.values = [titles];
+          startCell = startCell.getCell(1, 0);
+        }
+        const detail = startCell.getResizedRange(
+          this.props.result.Data.length - 1,
+          this.props.result.Data[0].length - 1
+        );
+        detail.values = this.props.result.Data;
+        await context.sync();
+      });
+      // eslint-disable-next-line no-empty
+    } catch {}
+  };
+  handleInsert = async () => {
+    try {
+      // eslint-disable-next-line no-undef
+      await Excel.run(async (context) => {
+        const sheet = context.workbook.worksheets.add();
+        const range = sheet.getRange("A1").getResizedRange(0, this.props.result.Data[0].length - 1);
+        const table = sheet.tables.add(range, this.state.includeHeader);
+        if (this.state.includeHeader) {
+          const titles = Object.entries(this.props.result.Columns).map((c) => (c[1] === "" ? c[0] : c[1]));
+          table.getHeaderRowRange().values = [titles];
+        }
+        table.rows.add(null, this.props.result.Data);
+        sheet.getUsedRange().format.autofitColumns();
+        sheet.getUsedRange().format.autofitRows();
+        sheet.activate();
+        await context.sync();
+      });
+      // eslint-disable-next-line no-empty
+    } catch {}
+  };
+  render() {
+    let paste = <StackItem />;
+    let warning;
+    if (this.props.result !== null) {
+      if (this.props.result.Summary.moreRecords) {
+        warning = (
+          <StackItem align="start">
+            <MessageBar messageBarType={MessageBarType.warning}>Max records returned!</MessageBar>
+          </StackItem>
+        );
+      }
+      if (this.props.result.Summary.records > 0) {
+        paste = (
+          <Stack tokens={this.tokens}>
+            <StackItem align="start">
+              <MessageBar messageBarType={MessageBarType.info}>
+                Returned {this.props.result.Summary.records} row(s) with {this.props.result.Data[0].length} column(s)
+              </MessageBar>
+            </StackItem>
+            <Stack horizontal tokens={this.tokens}>
+              <PrimaryButton onClick={this.handlePaste}>Paste</PrimaryButton>
+              <PrimaryButton onClick={this.handleInsert}>Insert</PrimaryButton>
+            </Stack>
+            <StackItem align="start">
+              <Checkbox label="Include Header" onChange={this.handleIncludeHeader} checked={this.state.includeHeader} />
+            </StackItem>
+            {warning}
+          </Stack>
+        );
+      } else {
+        paste = (
+          <StackItem align="start">
+            <MessageBar messageBarType={MessageBarType.warning}>Zero records returned!</MessageBar>
+          </StackItem>
+        );
+      }
+    }
+    return paste;
+  }
+}
